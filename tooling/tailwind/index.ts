05cf7a9c10ebe49453c2e7b56534b4bc89a30a00
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2023-12-15 16:13:06 +0100

    use import

--- tooling/tailwind/index.ts
+++ tooling/tailwind/index.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import tailwindAnimate from "tailwindcss-animate"
 
 export default {
   content: [
@@ -82,5 +83,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 } satisfies Config
